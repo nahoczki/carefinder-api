41bddbc94230f7327b1de35035011f548f379c93
Zoli <nahoczki@uwp.edu>
2021-10-04 11:24:09 -0500

    Initial Commit

---
+++ src/main/kotlin/com/zolinahoczki/carefinderapi/CarefinderApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zolinahoczki.carefinderapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarefinderApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarefinderApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/zolinahoczki/carefinderapi/CarefinderApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zolinahoczki.carefinderapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarefinderApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
