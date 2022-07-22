3cf4b6c1be30820232a9f819ed671b5ac0da29a2
nahoczki <nnahoczki@gmail.com>
2022-07-22 17:01:36 -0500

    possible fix to cors error

--- src/main/kotlin/com/zolinahoczki/carefinderapi/CarefinderApiApplication.kt
+++ src/main/kotlin/com/zolinahoczki/carefinderapi/CarefinderApiApplication.kt
@@ -3,22 +3,10 @@ package com.zolinahoczki.carefinderapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class CarefinderApiApplication
 
 fun main(args: Array<String>) {
     runApplication<CarefinderApiApplication>(*args)
-
-    @Bean
-    fun configure(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**").allowedOrigins("*")
-            }
-        }
-    }
 }
