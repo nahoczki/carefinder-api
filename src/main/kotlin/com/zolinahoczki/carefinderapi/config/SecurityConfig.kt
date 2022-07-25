ae51734940d167a1671d549a391bc2003fbcd2ef
nahoczki <nnahoczki@gmail.com>
2022-07-24 20:15:18 -0500

    fix to cors issue

--- src/main/kotlin/com/zolinahoczki/carefinderapi/config/SecurityConfig.kt
+++ src/main/kotlin/com/zolinahoczki/carefinderapi/config/SecurityConfig.kt
@@ -2,15 +2,25 @@ package com.zolinahoczki.carefinderapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter()   {
 
     @Bean
     fun passwordEncoder() : PasswordEncoder {
         return BCryptPasswordEncoder(5)
     }
 
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+//        http.csrf().disable();
+        http.cors()
+    }
 }
--- src/main/kotlin/com/zolinahoczki/carefinderapi/filters/ApiKeyRequestFilter.kt
+++ src/main/kotlin/com/zolinahoczki/carefinderapi/filters/ApiKeyRequestFilter.kt
@@ -6,6 +6,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.filter.GenericFilterBean
 import java.io.IOException
 import javax.servlet.FilterChain
